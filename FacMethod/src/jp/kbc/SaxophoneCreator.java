347577a5ace0809d59fa4afb05e42e8d6aa9a53b
ninomiyatatsuya <kbc11a11@stu.kawahara.ac.jp>
2013-11-12 18:10:13 +0900

    Update SaxophoneCreator.java

--- FacMethod/src/jp/kbc/SaxophoneCreator.java
+++ FacMethod/src/jp/kbc/SaxophoneCreator.java
@@ -1,5 +1,8 @@
 package jp.kbc;
 
-public class SaxophoneCreator {
-
+public class SaxophoneCreator extends AbstructCreator{
+  @Override
+  public WindInstrumentProduct createInstrument(){
+    return new Saxophone();
+  }
 }
